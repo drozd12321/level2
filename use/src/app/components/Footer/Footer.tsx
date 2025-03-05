d9479f8e9567cec6741e673e4ecefa6fce379469
Drozdov Evgeny <drozdove912@gmail.com>
2025-03-05 16:18:50 +0200

    env

--- use/src/app/components/Footer/Footer.tsx
+++ use/src/app/components/Footer/Footer.tsx
@@ -1,13 +1,24 @@
 import React, { DetailedHTMLProps, HTMLAttributes } from "react";
 import styles from "./Footer.module.css";
+import { format } from "date-fns";
 interface FooterProps
   extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {}
 const Footer = ({ className, ...props }: FooterProps) => {
   return (
     <footer className={`${className} ${styles.fot}`}>
-      <div>Lorem ipsum dolor sit amet consectetu</div>
-      <div>Ссылка 1</div>
-      <div>Ссылка 2</div>
+      <div>
+        Lorem ipsum dolor {format(new Date(), "dd.mm.yyyy")} sit amet consectetu
+      </div>
+      <div>
+        <a href="#" target="_blank">
+          Пользовательское соглашение
+        </a>
+      </div>
+      <div>
+        <a href="#" target="_blank">
+          Политика конфиденциальности
+        </a>
+      </div>
     </footer>
   );
 };

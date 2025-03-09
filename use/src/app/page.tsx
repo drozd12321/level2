a1c8ed24468c507f7630cb3361878cd5f714e680
Drozdov Evgeny <drozdove912@gmail.com>
2025-03-09 13:07:59 +0300

    1

--- use/src/app/page.tsx
+++ use/src/app/page.tsx
@@ -1,12 +1,7 @@
-import getMenu from "@/data/getMenu";
 export default async function Home() {
-  const menu = await getMenu();
-
   return (
     <main>
-      {menu.map((men) => (
-        <li key={men._id.secondCategory}>{men._id.secondCategory}</li>
-      ))}
+      <div>ALL INFO</div>
     </main>
   );
 }
